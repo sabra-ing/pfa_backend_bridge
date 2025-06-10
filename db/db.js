9bd99502a05651fbe66499219b5bda0492b47ee4
sabra-ing <benfadhelsabra592@gmail.com>
2025-06-10 15:30:02 +0100

    securite

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 module.exports.connectToMongoDB = async () =>{
     mongoose.set("strictQuery",false)
-    mongoose.connect("mongodb://localhost:27017/summercamp").then(
+    mongoose.connect(process.env.Mongo_Url).then(
         ()=>{
             console.log("connect to db")
         } 
